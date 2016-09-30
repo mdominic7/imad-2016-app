ad6d3a6c439e5ab28a653aac409bdf2e0443ca49
mdominic7 <mdominic7@gmail.com>
2016-09-30 21:37:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/myprofile', function (req, res) {
-  res.sendFile(path.join(_dirname, 'master','1.html'));
+  res.sendFile(path.join('1.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
