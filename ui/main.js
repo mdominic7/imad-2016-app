e248b6413d31039623788b595f105f5eabcccf07
mdominic7 <mdominic7@gmail.com>
2016-10-11 20:36:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,23 @@
+console.log('Loaded Up!');
+alert("welocme Guys!!!");
+/*var element = document.getElementById('main-text')
+element.innerHTML = 'Hello! the content has been changed through JS!!!'*/
 
+// making an image move by 100px to right
+var element2 = document.getElementById('Mypic');
+
+var marginLeft=0;
+
+function moveRight()
+{
+    marginLeft = marginLeft + 5;
+    element2.style.marginLeft = marginLeft + 'px';
+}
+
+element2.onclick = function(){
+    var interval = setInterval(moveRight,30);
+
+}
 var button = document.getElementById('counter');
 button.onClick = function()
 {
