9d2cdb2b49941a2cf2a84f5a22fd9688aca70892
mdominic7 <mdominic7@gmail.com>
2016-10-11 20:25:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,32 @@ function moveRight()
 element2.onclick = function(){
     var interval = setInterval(moveRight,30);
 
+}
+var button = document.getElementById('counter');
+button.onClick = function()
+{
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the request
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            //Take some action
+            if(request.status === 200)   //succesful request
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+            //not done yet
+        }
+        
+    };
+    
+    //make the rquest
+    request.open('GET', 'http://mdominic7.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
-
 
 
