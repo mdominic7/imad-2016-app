0ba4700a1d131479392185bd3cb1301ff4a29d9d
mdominic7 <mdominic7@gmail.com>
2016-10-11 17:41:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded Up!');
 alert("welocme Guys!!!");
 var element = document.getElementById('main-text')
 element.innerHTML = 'Hello! the content has been changed through JS!!!'
+
+// making an image move by 100px to right
+var element2 = document.getElementById('Mypic');
+element2.onclick = function(){
+    element2.style.marginLeft = '100px';
+};
