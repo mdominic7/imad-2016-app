78506367944a858605b56f0279da137ba64a7ea9
mdominic7 <mdominic7@gmail.com>
2016-10-21 13:40:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ var counter = 0;
 button.onClick = function()
 {
     //create a request object
-    var request = new XMLHttpRequest();
+  /*  var request = new XMLHttpRequest();
     
     //capture the request
     request.onreadystatechange = function(){
@@ -32,18 +32,19 @@ button.onClick = function()
             //Take some action
             if(request.status === 200)   //succesful request
             {
-                var counter = request.responseText;
+                var counter = request.responseText;*/
+                counter = counter + 1;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-            }
+            
             //not done yet
-        }
+        
         
     };
     
     //make the rquest
     request.open('GET', 'http://mdominic7.imad.hasura-app.io/counter',true);
     request.send(null);
-};
+
 
 
